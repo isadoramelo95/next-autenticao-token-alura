19006ec66b625393e9e7de88f6089938ab5c9238
isado <isado@LAPTOP-UC5IQEQ6>
2024-07-16 14:54:05 -0300

    aula 1 e 2 alura

---
+++ frontend/pages/auth-page-ssr.js
@@ -0,0 +1,28 @@
+import nookies from 'nookies';
+import { tokenService } from '../src/service/auth/tokenService';
+
+function AuthPageSSR(props) {
+  return (
+    <div>
+      <h1>
+        Auth Page Server Side Render
+      </h1>
+      <pre>
+        {JSON.stringify(props, null, 2)}
+      </pre> 
+    </div>
+  )
+}
+
+export default AuthPageSSR;
+
+export async function getServerSideProps(ctx) {
+  const cookies = nookies.get(ctx)
+  console.log('cookies', cookies);
+
+  return {
+    props: {
+      token: tokenService.get(ctx),
+    },
+  }
+}
